19004a94dadc3011c11762f10d0f0e64728f40a0
sathvi3 <sathvikbhandar007@hotmail.com>
2021-07-04 18:06:15 +0530

    Pagination fix.

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -12,7 +12,7 @@ const Pagination = ({ charactersPerPage, totalCharacters, paginate }) => {
       <ul className="pagination justify-content-center">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="!#" className="page-link">
+            <a onClick={() => paginate(number)} href="#" className="page-link">
               {number}
             </a>
           </li>
